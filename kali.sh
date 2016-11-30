3a880efe8124474e20a5fe5e1736fddc2cd55f03
Dewey Hong <dhong@fortinet.com>
2016-11-29 16:05:48 -0800

    .

--- kali.sh
+++ kali.sh
@@ -4,8 +4,12 @@ set -x
 
 #vagrant init Sliim/kali-linux-2.0-amd64
 
+rm -Rf discover
+mkdir discover
+
 vagrant destroy -f
 vagrant up
 
+vagrant ssh
 
 exit 0
--- scripts/kali.sh
+++ scripts/kali.sh
@@ -3,18 +3,33 @@
 set -x
 
 # change hosts
-sudo echo '' >> /etc/hosts
-sudo echo '# for vm' >> /etc/hosts
+echo '' >> /etc/hosts
+echo '# for vm' >> /etc/hosts
 
-sudo echo "Reading config...." >&2
-source /vagrant/setup.rc
+echo '' >> .bashrc
+echo "alias ll='ls -al'" >> .bashrc
+echo "export HOME='/vagrant'" >> .bashrc
+source .bashrc
 
 export LC_ALL=C
 export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-export HOME=/home/vagrant
+export HOME=/vagrant
+
+mv /etc/apt/sources.list /etc/apt/sources.list_bak
+touch /etc/apt/sources.list
+echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list
+apt-get update
+
+git clone https://github.com/n0needt0/discover /vagrant/discover/
+ln -s /vagrant/discover /opt/discover
+
+cd /opt/discover/
+./update.sh
+
+# ./discover.sh
 
 exit 0
 
